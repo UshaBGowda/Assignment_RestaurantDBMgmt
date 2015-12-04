d08db8f189babe28098b46423632adb5c15ac96e
UshaBGowda <usha.b03@gmail.com>
2015-12-03 17:23:36 -0800

    First Checkin

---
+++ sqlDataSourceDemo/Default.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace sqlDataSourceDemo
+{
+    public partial class Default : System.Web.UI.Page
+    {
+ 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void restaurantButton_Click(object sender, EventArgs e)
+        {
+            RestaurantGrid.Visible = true;
+            RestaurantDetailView.Visible = true;
+        }
+
+        protected void customerButton_Click(object sender, EventArgs e)
+        {
+            customerGrid.Visible = true;
+            customerDetailsView.Visible = true;
+        }
+
+          protected void tableButton_Click(object sender, EventArgs e)
+        {
+            tableGrid.Visible = true;
+            tableDetailsView.Visible = true;
+        }
+
+          protected void reservationButton_Click(object sender, EventArgs e)
+        {
+            reservationGrid.Visible = true;
+            ReservationDetailView.Visible = true;
+        }
+
+         protected void DishButton_Click(object sender, EventArgs e)
+        {
+            DishGrid.Visible = true;
+            DishDetailsView.Visible = true;
+        }
+        
+        
+    }
+}
---
+++ sqlDataSourceDemo/Header/Header.ascx
@@ -0,0 +1,6 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Header.ascx.cs" Inherits="sqlDataSourceDemo.Header.Header" %>
+<div style="font-size:18pt; color:blue; align-content:center" > &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; FOODIE TEAM : Restaurant Database 
+<asp:Label id="HeaderLabel" runat="server"></asp:Label> 
+    <br />
+    <br />
+</div>
